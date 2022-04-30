bc1ee243121396a2861302d3e9fbd518d137694b
kvelraj_concord <kvelraj@concord.net>
2022-04-30 12:03:18 +0530

    JS

--- HTML/program.js
+++ HTML/program.js
@@ -23,3 +23,27 @@ console.log(future_value);
 
 user_email = "kumar@yahoo.in";
 console.log(user_email);
+
+var first_name = "Karthick";
+var last_name = "Kumar";
+var age = 28;
+var roll_no = 487655566;
+
+// Object -> It is collection of Key and Values
+
+// var variable_name = {
+//   key1 : value,
+//   key2 : value
+// }
+
+// key -> Similar to variable_name -> it should contains a-z, A-Z, 0-9, _, $
+// value -> Number, String, Boolean, Object, Array, Function
+
+var studentInfo = {
+  first_name : "Karthick",
+  last_name : "kumar",
+  age : 28,
+  roll_no : 487655566
+};
+
+console.log(studentInfo)
