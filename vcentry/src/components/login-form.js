78aa2efe8867f48e8ad7094fee7c2f671dac4f45
kvelraj_concord <kvelraj@concord.net>
2022-05-14 11:57:09 +0530

    react app

---
+++ vcentry/src/components/login-form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LoginAction extends Component {
+
+  render(){
+    return (
+      <div>
+        <h1>Login Page Action</h1>
+      </div>
+    )
+  }
+}
+
+export default LoginAction;
