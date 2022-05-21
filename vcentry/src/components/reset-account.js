d5b36075f7108bbf8366c79d2efc02c9f7c25678
kvelraj_concord <kvelraj@concord.net>
2022-05-21 11:16:42 +0530

    functional comp

--- vcentry/src/components/reset-account.js
+++ vcentry/src/components/reset-account.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ResetAccount = () => {
+  const username = "karthick kumar";
+
+  // const [variableName, methodName] = useState(intialValue);
+  const [studentName, setStudentName] = useState("Mr.William");
+
+  const updateStudentName = () => {
+    const value = prompt("Enter the student name");
+
+    // To update a useState -> methodName(newValue);
+    setStudentName(value);
+  }
+
   return(
     <div>
-      <h1>This is Reset Account</h1>
+      <h1>This is Reset Account {username}</h1>
+      <h1>The student Name is {studentName}</h1>
       <div className="space">
           <label className="lable">Enter your Username</label>
           <input className="inputbox" type="text" placeholder="Pls enter username"/>
@@ -13,6 +26,8 @@ const ResetAccount = () => {
           <input className="inputbox" type="password" placeholder="Pls enter password"/>
         </div>
         <button className="button">Reset</button>
+        <br></br>
+        <button onClick={() => updateStudentName()}>Update Student Name</button>
     </div>
   )
 }
