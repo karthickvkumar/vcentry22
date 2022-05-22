2ccc1026fc3e55696db2eb3e186cc236ca6b73a8
kvelraj_concord <kvelraj@concord.net>
2022-05-22 11:47:31 +0530

    routing

--- vcentry/src/App.js
+++ vcentry/src/App.js
@@ -1,18 +1,24 @@
-// import LoginAction from "./components/login-form";
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import LoginAction from "./components/login-form";
 import ResetAccount from "./components/reset-account";
-// import RegisterForm from "./components/register-form";
+import RegisterForm from "./components/register-form";
 import ImageContent from "./components/image-content";
+import LandingContent from "./components/landing-content";
 import "./css/style.css";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to React JS Application</h1>
-      {/* <LoginAction></LoginAction> */}
-      <ResetAccount></ResetAccount>
-      {/* <RegisterForm></RegisterForm> */}
-      {/* <ImageContent></ImageContent> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingContent></LandingContent>}></Route>
+        <Route path="login" element={<LoginAction></LoginAction>}></Route>
+        <Route path="reset" element={<ResetAccount></ResetAccount>}></Route>
+        <Route path="register" element={<RegisterForm></RegisterForm>}></Route>
+        <Route path="image" element={<ImageContent></ImageContent>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ vcentry/src/components/landing-content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingContent = () => {
+  return (
+    <div>
+      <h1>Welcome to React Application</h1>
+    </div>
+  );
+};
+
+export default LandingContent;<h1>Welcome to React Application</h1>
