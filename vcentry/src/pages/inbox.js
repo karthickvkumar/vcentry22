37fa209b189a60c7d3e5bdc2ee2dc6270aa85380
kvelraj_concord <kvelraj@concord.net>
2022-05-28 12:19:19 +0530

    get api

--- vcentry/src/pages/inbox.js
+++ vcentry/src/pages/inbox.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 const InboxPage = () => {
+
+  const loadUserProfile = () => {
+    const url = "https://reqres.in/api/users?page=2";
+    //method chaining
+    axios.get(url)
+          .then((success) => {
+            //succes code
+            let response = success.data;
+            console.log(response);
+          })
+          .catch((error) => {
+            //failure code
+            console.log(error);
+          })
+  }
+
   return (
     <div>
       <h1>This is a Inbox Page</h1>
+      <button onClick={() => loadUserProfile()}>Load User Profile</button>
     </div>
   );
 };
