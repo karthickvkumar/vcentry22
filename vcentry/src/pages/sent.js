9a46e885f3d6f586fc8dc543a10e6895d9ab0fab
kvelraj_concord <kvelraj@concord.net>
2022-05-29 12:32:48 +0530

    post and put method

--- vcentry/src/pages/sent.js
+++ vcentry/src/pages/sent.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {useNavigate} from "react-router-dom"
 
 const SentPage = () => {
+
+  const navigate = useNavigate();
+  const [register, setRegister] = useState({
+    email : "",
+    password : ""
+  })
+
+  const onHandleInput = (event) => {
+    setRegister({...register, [event.target.name] : event.target.value});
+  }
+
+  const submitRegister = () => {
+    const url = "https://reqres.in/api/register";
+
+    axios.post(url, register)
+        .then((success) => {
+          console.log(success);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error.message);
+        })
+
+  }
+
   return (
     <div>
       <h1>This is a Sent Page</h1>
+      <div className="space">
+          <label className="lable">Enter your Email ID</label>
+          <input className="inputbox" type="text" placeholder="Pls enter email id"  onChange={ onHandleInput } name="email"/>
+        </div>
+        <div className="space">
+          <label className="lable">Enter your Password</label>
+          <input className="inputbox" type="password" placeholder="Pls enter password" onChange={ onHandleInput } name="password"/>
+        </div>
+        <button onClick={() => submitRegister()}>Register</button>
     </div>
   );
 };
--- vcentry/src/pages/stared.js
+++ vcentry/src/pages/stared.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const StaredPage = () => {
+
+  const [user, setUser] = useState({
+    name : "",
+    job : ""
+  });
+
+  const onHandleInput = (event) => {
+    setUser({...user, [event.target.name] : event.target.value});
+  }
+
+  const submitUser = () => {
+    const url = "https://reqres.in/api/users/2";
+
+    axios.put(url, user)
+        .then((success) => {
+          const response = success.data;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          
+        })
+
+  }
+
   return (
     <div>
       <h1>This is a Started Page</h1>
+      <div className="space">
+          <label className="lable">Enter your Name</label>
+          <input className="inputbox" type="text" placeholder="Pls enter name"  onChange={ onHandleInput } name="name"/>
+        </div>
+        <div className="space">
+          <label className="lable">Enter your Job</label>
+          <input className="inputbox" type="text" placeholder="Pls enter job" onChange={ onHandleInput } name="job"/>
+        </div>
+        <button onClick={() => submitUser()}>Submit User</button>
     </div>
   );
 };
