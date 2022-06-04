c528357abd7f112d3268d4135561519363469b0f
kvelraj_concord <kvelraj@concord.net>
2022-06-04 11:24:24 +0530

    component

--- vcentry/src/pages/child.js
+++ vcentry/src/pages/child.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const ChildComponent = (props) => {
-    
+  
+  const onHandleInput = (event) => {
+    // console.log(event.target.value);
+    props.callbackAttributeName(event.target.value);
+  }
+
   return (
     <div>
       <h1>This is a Child Component</h1>
       <p style={{fontSize : "20px"}}>{props.attributeName}</p>
+      <div>
+        <label>Enter your text:</label>
+        <input type="text" placeholder="Enter any text.." onChange={onHandleInput}/>
+      </div>
     </div>
   );
 };
--- vcentry/src/pages/parent.js
+++ vcentry/src/pages/parent.js
@@ -1,23 +1,32 @@
 import React, {useState} from 'react';
+import { useSearchParams } from 'react-router-dom';
 import ChildComponent from './child';
 
 const ParentComponent = () => {
 
   const [textMessage, setTextMessage] = useState("");
+  const [incomingMsg, setIncomingMsg] = useState("");
 
   const onHandleTextArea = (event) => {
     setTextMessage(event.target.value);
+  }
+
+  const callbackMethod = (text) => {
+    setIncomingMsg(text);
   }
 
   return (
     <div>
       <h1>This is a Parent Component</h1>
       <div>
+        <p>Incoming message -> {incomingMsg}</p>
+      </div>
+      <div>
         <label>Enter you Information :</label>
         <br></br>
         <textarea placeholder='type here...' className='textarea' onChange={onHandleTextArea}></textarea>
       </div>
-      <ChildComponent attributeName={textMessage}></ChildComponent>
+      <ChildComponent attributeName={textMessage} callbackAttributeName={callbackMethod}></ChildComponent>
     </div>
   );
 };
