6c2c940ebaf5aec05d7c84e39377a27aa9169a88
kvelraj_concord <kvelraj@concord.net>
2022-06-18 11:59:49 +0530

    post api

--- NodeApi/server.js
+++ NodeApi/server.js
@@ -27,8 +27,52 @@ connection.connect((error) => {
   }
 
   console.log("MYSQL database is connected successfully");
-})
+});
 
+// http://localhost:4000/api/users/list
+app.get("/api/users/list", (request, response) => {
+  const sql_query = `SELECT * FROM Karthick_Kumar`;
+  
+  connection.query(sql_query, (error, result) => {
+    if(error){
+      response.status(500).send({
+        error,
+        message : "Sorry something went wrong in server, pls try again!"
+      });
+      return;
+    }
+
+    response.status(200).send(result);
+
+  })
+});
+
+// http://localhost:4000/api/user/create
+app.post("/api/user/create", (request, response) => {
+  const firstName = request.body.first_name;
+  const lastName = request.body.last_name;
+  const rollNumber = request.body.roll_number;
+  const _location = request.body.location;
+  const result = request.body.result;
+
+  const sql_query = `INSERT INTO Karthick_Kumar (first_name, last_name, roll_number, location, result) VALUES ('${firstName}', '${lastName}', ${rollNumber}, '${_location}', ${result})`;
+
+  connection.query(sql_query, (error, result) => {
+    if(error){
+      response.status(500).send({
+        error,
+        message: "Sorry something went wrong in server, pls try again!"
+      });
+      return;
+    }
+
+    response.status(200).send({
+      result,
+      message : "Successfully User Profile has been Created"
+    })
+  })
+
+})
 
 
 const port = process.env.PORT || 4000;
